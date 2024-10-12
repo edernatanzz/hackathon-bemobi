73425146af82b42c587b8a9f689ad8abbaf630f0
EderNatan.dev <edernatan124@gmail.com>
2024-10-12 16:21:49 -0300

    componets

--- frontend-next/src/app/home/page.tsx
+++ frontend-next/src/app/home/page.tsx
@@ -1,9 +1,19 @@
- import React from "react"
+ import Assinaturas from "@/components/homeAssinaturas"
+import React from "react"
+import styled from "styled-components"
 
+
+ const Name = styled.div `
+ 
+ `
  export default function HomePage(){
    return(
-
-      <h1>tfadsfadsfad</h1>
+      <>
+      <Name></Name>
+      <Assinaturas/>
+      
+      
+      </>
    )
    }
     
---
+++ frontend-next/src/components/homeAssinaturas.tsx
@@ -0,0 +1,6 @@
+'use client'
+export default function Assinaturas(){
+    return(
+        <h1>fdsdas</h1>
+    )
+}
